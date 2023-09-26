a27014d87d56697b92aac8889e712228cbbc039d
zeinab <zeinabmostafa630@gmail.com>
2023-09-25 19:34:52 -0700

    fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A c program that prints with put function
+ *
+ * Return: always 0 (success)
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
